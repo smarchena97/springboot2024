c2f6032ec381a93d50602b456a680a41730ddbab
= <=>
2024-01-30 19:17:32 -0500

    se crea la estructura del proyecto

---
+++ simpleApiRest/src/main/java/com/apirest/simpleApiRest/SimpleApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.simpleApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiRestApplication.class, args);
+	}
+
+}
---
+++ simpleApiRest/src/test/java/com/apirest/simpleApiRest/SimpleApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.simpleApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
